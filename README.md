2a853ad1ff9f95835e48f7e3d7c260e85233b8ff
Ben Francis <ben@tola.me.uk>
2017-03-10 19:46:04 +0000

    Serve static files with Express

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Webian Home
-Home Web Server
+Home web server
+
+## Get Started
+Make sure that you have a recent version of [NodeJS](https://nodejs.org) installed.
+
+Clone the GitHub repository (or fork it first):
+
+```$ git clone https://github.com/webianproject/home.git```
+
+Change into the gateway directory:
+
+```$ cd home```
+
+Install dependencies:
+
+```$ npm install```
+
+ Start the web server:
+
+```$ node app.js```
+
+Load ```http://localhost:8080``` in your web browser.
---
+++ app.js
@@ -0,0 +1,26 @@
+/*
+© Ben Francis 2017
+
+This file is part of Webian Home.
+
+Webian Home is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Webian Home is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Webian Home.  If not, see <http://www.gnu.org/licenses/>.
+*/
+var express = require('express');
+var app = express();
+
+app.use(express.static('static'));
+
+app.listen(8080, function () {
+  console.log('Listening on port 8080.');
+});
