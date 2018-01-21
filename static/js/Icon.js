34941e032078d2e280979266468512e0422c8422
Ben Francis <ben@tola.me.uk>
2018-01-21 19:32:54 +0000

    Make icons hyperlinks rather than using window.open

--- static/js/Icon.js
+++ static/js/Icon.js
@@ -30,42 +30,16 @@ var Icon = function(siteObject, target, pinned) {
  */
 Icon.prototype.view = function() {
   var style = 'background-image: url(' + this.iconUrl +  ');';
-  return '<li id="icon-' + this.id +'" class="icon" style="' +
-  style + '"><span class="icon-name">' + this.name + '</span></li>';
+  return '<a href="' + this.startUrl + '" target="_blank"><li id="icon-' +
+  this.id +'" class="icon" style="' + style + '"><span class="icon-name">' +
+  this.name + '</span></li></a>';
 };
 
 /**
  * Render Icon.
  *
- * @param {String} target Open in current window (_self) or a new one (_blank).
- * @param {Boolean} pinned Whether or not the site is pinned.
  */
-Icon.prototype.render = function(target, pinned) {
+Icon.prototype.render = function() {
   this.container.insertAdjacentHTML('beforeend', this.view());
   this.element = document.getElementById('icon-' + this.id);
-  if (target && target == '_self') {
-    this.element.addEventListener('click', this.navigate.bind(this));
-  } else {
-    this.element.addEventListener('click', this.open.bind(this));
-  }
-  if (pinned) {
-    this.element.classList.add('pinned');
-  }
-};
-
-/**
- * Launch site in a new window.
- */
-Icon.prototype.open = function() {
-  var features = 'siteId=' + this.id;
-  window.open(this.startUrl, '_blank', features);
-  console.log('opening window at ' + this.startUrl);
-};
-
-/**
- * Navigate to site in current window.
- */
-Icon.prototype.navigate = function() {
-  window.location.href = this.startUrl;
-  console.log('navigating to ' + this.startUrl);
 };
